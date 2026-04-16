1e7242f5a9454f59d4635252fcf972e097aaed58
Dom Wenskay <dominicmwenskay@gmail.com>
2026-04-15 20:26:16 -0400

    hopper out changed :)

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -41,7 +41,7 @@ public class Hopper extends SubsystemBase {
     private Drivetrain _drivetrain;
     private int _timer = 0; 
     private final PositionDutyCycle INTAKE_IN = new PositionDutyCycle(.01);
-    private final PositionDutyCycle INTAKE_OUT = new PositionDutyCycle(16.6);
+    private final PositionDutyCycle INTAKE_OUT = new PositionDutyCycle(16);//.6
     private final PositionDutyCycle INTAKE_SHOOT = new PositionDutyCycle(7.4);
 
     public void setWantedState(HopperState wantedState) {
